968039238f86ab8ef9952bd1d0e9a15c0d7db8f2
LeeSeungJae <coderoom2018@gmail.com>
2019-09-29 13:01:18 +0900

    add airci

---
+++ __test__/test.js
@@ -0,0 +1,4 @@
+describe('self-assessments-02', function() {
+  require('../algo-time-complexity/algo-time-complexity.test')
+  require('../js-inheritance-pseudo-classical-alt/js-inheritance-pseudo-classical-alt.test')
+});
